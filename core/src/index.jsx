951306b9dc890bf9ba1c0912df5133eb50ed372a
Duc Filan <ducfilan@gmail.com>
2025-10-03 17:43:10 +0800

    Add options for direction

--- core/src/index.jsx
+++ core/src/index.jsx
@@ -4,7 +4,7 @@ import { View } from './view'
 import css from './index.module.scss'
 import { SvgMenu, SvgZoomIn, SvgZoomOut } from './icon'
 
-export function useOnnx (url) {
+export function useOnnx(url) {
     const [data, setData] = useState(null)
     useEffect(() => {
         console.log(url)
@@ -17,7 +17,7 @@ export function useOnnx (url) {
     return data
 }
 
-const ReactOnnx = forwardRef(({ width, height, file }, ref) => {
+const ReactOnnx = forwardRef(({ width, height, file, direction }, ref) => {
     const refIns = useRef(null)
     const refFile = useRef(null)
 
@@ -26,6 +26,13 @@ const ReactOnnx = forwardRef(({ width, height, file }, ref) => {
         refIns.current = new View(host)
         refIns.current?.start()
     }, [])
+
+    useEffect(() => {
+        if (refIns.current && direction) {
+            refIns.current.setDirection(direction);
+        }
+    }, [direction]);
+
 
     useEffect(() => {
         if (file) {
@@ -39,11 +46,11 @@ const ReactOnnx = forwardRef(({ width, height, file }, ref) => {
         }
     }, [])
 
-    function doOpen () {
+    function doOpen() {
         refFile.current?.click()
     }
 
-    function doLoadByFile ({ target }) {
+    function doLoadByFile({ target }) {
         const files = Array.from(target.files)
         const file = files[0]
         refIns.current?.openByFile(file, files)
@@ -53,11 +60,11 @@ const ReactOnnx = forwardRef(({ width, height, file }, ref) => {
     return (
         <>
             <input ref={refFile} type="file" multiple={false} accept=".onnx" onChange={doLoadByFile}
-                   style={{ display: 'none' }}/>
+                style={{ display: 'none' }} />
             <section className={css.onnxGraph} style={{ width: width || '100%', height: height || 400 }}>
                 <div id="graph" className="graph" tabIndex="0">
                     <svg id="canvas" className="canvas" preserveAspectRatio="xMidYMid meet" width="100%"
-                         height="100%"></svg>
+                        height="100%"></svg>
                 </div>
                 <div id="sidebar" className="sidebar">
                     <h1 id="sidebar-title" className="sidebar-title"></h1>
@@ -66,20 +73,20 @@ const ReactOnnx = forwardRef(({ width, height, file }, ref) => {
                 </div>
                 <div id="toolbar" className="toolbar">
                     <button id="sidebar-button" className="toolbar-button" title="Model Properties">
-                        <SvgMenu/>
+                        <SvgMenu />
                     </button>
                     <button id="zoom-in-button" className="toolbar-button" title="Zoom In">
-                        <SvgZoomIn/>
+                        <SvgZoomIn />
                     </button>
                     <button id="zoom-out-button" className="toolbar-button" title="Zoom Out">
-                        <SvgZoomOut/>
+                        <SvgZoomOut />
                     </button>
                     <button id="back-button" className="toolbar-back-button" title="Back">
                         &#x276E;
                     </button>
-                    <button id="name-button" className="toolbar-name-button" title="Name"/>
+                    <button id="name-button" className="toolbar-name-button" title="Name" />
                 </div>
-                <div id="menu" className="menu"/>
+                <div id="menu" className="menu" />
                 <div id="menu-button" className="menu-button">&#9776;</div>
             </section>
         </>
